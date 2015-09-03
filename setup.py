886ff89c32d864f3591cffe11e57b19df70cea40
Brian Dolbec <dolsen@gentoo.org>
2015-09-03 09:04:44 -0700

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys
+
+from distutils.core import setup
+from pyGPG import __version__, __license__
+# this affects the names of all the directories we do stuff with
+sys.path.insert(0, './')
+
+#__version__ = os.getenv('VERSION', default='9999')
+
+setup(
+    name='pyDeComp',
+    version=__version__,
+    description="A Python library for subprocessing various compression "
+        "and decompression routines.  Including generating contents listings",
+    author='Brian Dolbec',
+    author_email='dolsen@gentoo.org',
+    url="https://github.com/dol-sen/pyDeComp",
+    packages=['DeComp'],
+    license=__license__,
+    long_description=open('README').read(),
+    keywords='archive',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: OS Independent',
+        'Topic :: Compression',
+    ],
+)
